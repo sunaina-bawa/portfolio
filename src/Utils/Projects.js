1b7099ab61dcbed8cf93d608ca02e6163f94a1d4
sunaina <bawasunaina6@gmail.com>
2023-04-05 00:17:16 +0530

    portfolio

---
+++ src/Utils/Projects.js
@@ -0,0 +1,29 @@
+export const projects = [
+  {
+    img: "https://site-images.similarcdn.com/image?url=boat-lifestyle.com&t=1&s=1&h=382a6479da4ca0ca6585140a35fb597e7f0e0c597baf3b1516c575f2ac817fee",
+    title: "boAt Lifestyle-Clone",
+    des: "BoAt is an India-based consumer electronics brand established in 2015 that markets earphones, headphones stereos, travel chargers and premium rugged cables.This was a collaborative project built by team of 4 members in 5 days.",
+    demo: "",
+    live: "https://poetic-quokka-d75aa0.netlify.app/",
+    gitHub: "https://github.com/bharatvishvkarma/boAt-lifeStyle-Clone",
+    technologies: [
+      "HTML",
+      "CSS",
+      "JavaScript"
+     
+      
+    ],
+  },
+  {
+    img: "https://i0.wp.com/www.smartprix.com/bytes/wp-content/uploads/2021/02/Screenshot-2021-02-11-131356.png?fit=696%2C283&ssl=1",
+    title: "kooapp-Clone",
+    des: "Koo is an Indian microblogging and social networking service, based in Bengaluru, India. As of November 2022, the company is valued at over $275 million. The microblogging site was co-founded by entrepreneurs Aprameya Radhakrishna and Mayank Bidawatka. This was a collaborative project buit by team of 5 members in 5 days.",
+    
+    demo: "",
+    live: "https://koo-app-1.netlify.app/",
+    gitHub: "https://github.com/samrat124/Koo-AppClone",
+    technologies: ["HTML", "CSS", "JavaScript","React", "Chakra UI"],
+  },
+  
+ 
+  ];
